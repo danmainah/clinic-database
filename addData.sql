38459a22235456b55f574eed6fb481fcb6edf6e6
unknown <danielmainawaweru@gmail.com>
2021-10-28 10:50:11 +0300

    Add schema and data files

---
+++ addData.sql
@@ -0,0 +1,5 @@
+-- add data to database
+
+INSERT INTO animals (name) VALUES ('Agumon'), ('Gabumon'), ('Pikachu'), ('Devimon'), ('Charmander'), ('Plantmon'), ('Squirtle'), ('Angemon'), ('Boarmon'), ('Blossom');
+
+INSERT INTO vets (name) VALUES ('William Tatcher'), ('Maisy Smith'), ('Stephanie Mendez'), ('Jack Harkness');
